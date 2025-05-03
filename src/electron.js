92ca1fe1bcee1832b3440f1c2ea97622906fee30
biter0o1 <biter0o1@o2.pl>
2025-05-03 10:05:35 +0200

    move style css, adjust electron

--- src/electron.js
+++ src/electron.js
@@ -1,14 +1,18 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 1920,
-    height: 1080
-    // fullscreen: true
-  });
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 1920,
+		height: 1080,
+		// fullscreen: true,
+		// webPreferences: {
+		// 	nodeIntegration: true,
+		// 	contextIsolation: false,
+		// },
+	});
 
-  win.loadFile('index.html');
+	win.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
